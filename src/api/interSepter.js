c527a15687e1546ad99dd70c5619b468cc1039e7
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-10-25 17:05:03 +0530

    api

--- src/api/interSepter.js
+++ src/api/interSepter.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     // baseURL: import.meta.env.VITE_BASEURL
     baseURL:"https://blog-application-back-end.onrender.com",
-    baseURL: "http://localhost:3456"
+    // baseURL: "http://localhost:3456"
 });
 
 api.interceptors.request.use(
