7b0649423b96cc7424ff86ab0879f0fc86ce94d8
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-10-25 12:26:41 +0530

    api update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     images: {
-        domains: ['cdn-icons-png.flaticon.com', 'currentaffairs.adda247.com', 'www.citypng.com',"via.placeholder.com"], 
+        domains: ['cdn-icons-png.flaticon.com', 'currentaffairs.adda247.com', 'www.citypng.com', "via.placeholder.com"],
     },
 };
 
