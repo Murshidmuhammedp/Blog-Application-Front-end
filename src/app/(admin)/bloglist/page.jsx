218514a583c9c3195fffc4ccdcab177af9c30c68
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-10-25 12:12:18 +0530

    t3

--- src/app/(admin)/bloglist/page.jsx
+++ src/app/(admin)/bloglist/page.jsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation';
 
 const ListBlogPage = () => {
     const router = useRouter();
+    
     const [blogList, setBlogList] = useState([])
     const [refresh, setRefresh] = useState(false)
 
